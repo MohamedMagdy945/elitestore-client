59f9a93293a3cbb0c02ed15104f7814914dc8454
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-27 21:15:05 +0300

    chore: initalize tailwind and flowbit

---
+++ src/app/core/layouts/main-layout/main-layout.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Footer } from '../../../shared/components/footer/footer';
+import { Navbar } from '../../../shared/components/navbar/navbar';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { isPlatformBrowser } from '@angular/common';
+import { FlowbiteService } from '../../services/flowbite.service';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [
+    Navbar,
+    Footer
+  ],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css',
+})
+export class MainLayout implements OnInit {
+ constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
+}
