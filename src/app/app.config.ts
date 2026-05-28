bd14203024eba17b65ed4133fd3a006889edad8f
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-29 00:16:42 +0300

    feat: add spinner service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
+
 import { provideRouter } from '@angular/router';
 
+import {
+  provideClientHydration,
+  withEventReplay
+} from '@angular/platform-browser';
+
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { loadingInterceptor } from './core/interceptors/loading-interceptor';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+
+    provideRouter(routes),
+
+    provideClientHydration(withEventReplay()),
+
+    provideHttpClient(
+      withInterceptors([loadingInterceptor])
+    )
   ]
 };
