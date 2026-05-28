c23244c6bd622e62d63350b17d3c1bcef54f0f48
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-28 21:36:17 +0300

    feat: add login page first design

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+submitForm() {
+throw new Error('Method not implemented.');
+}
+   private readonly fb = inject(FormBuilder);
+
+
+   loginForm : FormGroup = this.fb.group({
+      email : ["" , [Validators.required , Validators.email]],
+      password : ["" , [Validators.required ,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]]
+    })
+  submit() {
+      console.log(this.loginForm.value);
+  }
+}
