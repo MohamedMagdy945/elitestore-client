666e22d06d4df5c8514634dbc63c51f44693c70c
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-06-01 10:03:09 +0300

    feat-auth: add login feature

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +9,32 @@ import { FormGroup, FormControl, ReactiveFormsModule, Validators, FormBuilder } 
   styleUrl: './login.css',
 })
 export class Login {
-submitForm() {
-throw new Error('Method not implemented.');
-}
-   private readonly fb = inject(FormBuilder);
+  private readonly fb = inject(FormBuilder);
+  private readonly authService = inject(AuthService);
 
+  loginForm: FormGroup = this.fb.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]]
+  })
 
-   loginForm : FormGroup = this.fb.group({
-      email : ["" , [Validators.required , Validators.email]],
-      password : ["" , [Validators.required ,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]]
-    })
-  submit() {
+  submitForm() {
+    if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      this.authService.login(this.loginForm.value)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            if (res.isSuccess) {
+              localStorage.setItem('token', res.data.accessToken);
+              console.log('Login Success');
+            } else {
+              console.log(res.message);
+            }
+          },
+          error: (err) => {
+            console.log('Error:', err);
+          }
+        });
+    }
   }
 }
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -51,7 +51,6 @@ export class Register {
             console.log('Error:', err);
           }
         });
-
     }
   }
 }
