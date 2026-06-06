3db9288ed3af9e5c9f37a60e99c3f01457681008
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-06-06 11:27:55 +0300

    feat: add product component initialization

---
+++ src/app/features/product/product.component.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product } from '../../core/models/product/Product.model';
+
+@Component({
+  selector: 'app-products.component',
+  imports: [RouterLink],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css',
+})
+export class ProductComponent {
+
+    productList = signal<Product[]>([])
+
+wishlistIds() {
+throw new Error('Method not implemented.');
+}
+addToWishlist(arg0: any) {
+throw new Error('Method not implemented.');
+}
+addToCart(arg0: any) {
+throw new Error('Method not implemented.');
+}
+}
